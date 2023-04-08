be7b71d01028705709434cfee5be4e8af9dcf358
gchang12 <gc8@hawaii.edu>
2023-04-07 23:07:27 -0700

    added new sections and so forth to 'output/'.

---
+++ typeset3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+output_dir="output/concepts";
+unalias cp;
+for kernel in {config,eplain,macros,fonts}; do (
+    cp kernel/$kernel.tex $output_dir;
+); done;
+cd $output_dir;
+for texfile in *.tex; do (
+    case "$texfile" in
+        "config.tex") false
+            ;;
+        "eplain.tex") false
+            ;;
+        "macros.tex") false
+            ;;
+        "fonts.tex") false
+            ;;
+        *) pdftex -output-format dvi -interaction batchmode $texfile;
+    esac;
+) done;
+rm *.aux *.idx *.log *.tex *.ccs;
