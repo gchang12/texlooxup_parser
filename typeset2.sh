798f9ea00919183d0467b45e20dbb5c57fabfd50
gchang12 <gc8@hawaii.edu>
2023-04-08 08:52:23 -0700

    examples section created in output/miscellany

--- typeset2.sh
+++ typeset2.sh
@@ -6,6 +6,7 @@ mkdir -p $miscellany_dir;
 for kernel in {config,eplain,macros,fonts}; do (
     cp kernel/$kernel.tex $miscellany_dir;
 ); done;
+cp kernel2/xmptext.tex $miscellany_dir;
 for section in {usebook,usingtex,examples,tips,errors,usermacs,capsule}; do (
     cp kernel2/$section.tex $miscellany_dir;
     cd $miscellany_dir;
