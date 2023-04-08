c7c70cc3f3b980f391d72956e4111584055b08a2
gchang12 <gc8@hawaii.edu>
2023-04-08 00:21:23 -0700

    Fixed logging with .sh files.

--- typeset2.sh
+++ typeset2.sh
@@ -9,7 +9,7 @@ for kernel in {config,eplain,macros,fonts}; do (
 for section in {usebook,usingtex,examples,tips,errors,usermacs,capsule}; do (
     cp kernel2/$section.tex $miscellany_dir;
     cd $miscellany_dir;
-    (echo "Now processing $section.tex"; pdftex -output-format dvi -interaction batchmode $section 1>/dev/null);
+    (echo "Now processing $section.tex"; pdftex -output-format dvi -interaction batchmode $section 1>/dev/null && echo "'$section.tex' processed successfully.");
     cd ../../;
 ); done;
 cd $miscellany_dir;
