ff46e7d8025e189805c02beb8c379569db9c65b3
gchang12 <gc8@hawaii.edu>
2023-04-11 21:40:09 -0700

    changed control-flow to optimize speed.

--- texdict.sh
+++ texdict.sh
@@ -1,38 +1,39 @@
-dvi_reader="okular";
 cd ./output/;
-
-# Determine whether or not to search by section.
-if [ "${1::1}" == "-" ]; then
-    fname_list=($(ls */*.dvi | grep -P ".*${1#-}.*(?=/)"));
-else
-    fname_list=($(ls */*.dvi | grep -P "$1(?=[^/]*\.dvi)"));
-fi;
-
-# Execute action based on parameter and resultset
+# Check if argument is null, then decide.
 if [ -z "$1" ]; then
     echo -e "\n    Please pass either a control word or section as an argument. e.g.\n";
-    echo "    'texdict par', 'texdict input', 'texdict (...)'";
+    echo "    'texdict par', 'texdict input', etc.";
     echo -e "    'texdict -{section}', where section is one of:\n$(ls | sed s/^/"        "/g)";
-elif [ -z "$fname_list" ]; then
-    printf "\"$1\" not found.";
 else
-    spacing=0;
-    space_factor=1;
-    while [[ "$space_factor" -lt ${#fname_list[@]} ]]; do
-        space_factor=$((space_factor*10)); spacing=$((spacing+1));
-    done;
-    echo;
-    echo "    View a definition";
-    echo "    =================";
-    for ((i=0; i<${#fname_list[@]}; i=i+1)); do
-        fname=${fname_list[$i]};
-        printf "    %*d: %s\n" $spacing $i ${fname%.dvi};
-    done;
-    echo;
-    read -p ">   Please make a selection: ";
-    fname=${fname_list[$REPLY]};
-    if [[ "$REPLY" =~ ^[0-9]+$ && -n "$fname" ]]; then
-        $dvi_reader $fname 2>/dev/null;
+    # Determine whether or not to search by section.
+    if [ "${1::1}" == "-" ]; then
+        fname_list=($(ls */*.dvi | grep -P ".*${1#-}.*(?=/)"));
+    else
+        fname_list=($(ls */*.dvi | grep -P "$1(?=[^/]*\.dvi)"));
+    fi;
+    # Check if selection list is empty, then decide.
+    if [ -z "$fname_list" ]; then
+        printf "\"$1\" not found.";
+    else
+        dvi_reader="okular";
+        spacing=0;
+        space_factor=1;
+        while [[ "$space_factor" -lt ${#fname_list[@]} ]]; do
+            space_factor=$((space_factor*10)); spacing=$((spacing+1));
+        done;
+        echo;
+        echo "    View a definition";
+        echo "    =================";
+        for ((i=0; i<${#fname_list[@]}; i=i+1)); do
+            fname=${fname_list[$i]};
+            printf "    %*d: %s\n" $spacing $i ${fname%.dvi};
+        done;
+        echo;
+        read -p ">   Please make a selection: ";
+        fname=${fname_list[$REPLY]};
+        if [[ "$REPLY" =~ ^[0-9]+$ && -n "$fname" ]]; then
+            $dvi_reader $fname 2>/dev/null;
+        fi;
     fi;
 fi;
 echo;
