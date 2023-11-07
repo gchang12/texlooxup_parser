b9049568fca1fb98a13e00bf093f48c6d86d4f61
gchang12 <gc8@hawaii.edu>
2023-11-07 09:45:01 -0800

    added a mock-texdict function, a test texdict module, some logging.

---
+++ test_query.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines functions that are meant to test the query.texdict function.
+"""
+
+import unittest
+import query
+
+class TexdictTest(unittest.TestCase):
+    """
+    """
+
+    def setUp(self):
+        """
+        """
+        pass
+
+
+if __name__ == '__main__':
+    pass
