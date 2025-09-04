31a5656077b0c5f59fc171c7d402a8185bf9ab82
gchang12 <gc8@hawaii.edu>
2025-09-04 15:47:50 -0700

    Reordered excerpt list

--- frontend/texlooxup_dict/public/entries/getexcerptlist.py
+++ frontend/texlooxup_dict/public/entries/getexcerptlist.py
@@ -1,13 +1,14 @@
-
 from pathlib import Path
 import json
 
 filelist = []
-target_file = "../../app/excerptList.ts"
 
 for excerptdir in filter(lambda p: p.is_dir(), Path('.').iterdir()):
     for excerptfile in excerptdir.iterdir():
         filelist.append((str(excerptdir), excerptfile.name))
 
-with open(target_file, encoding="utf-8", mode="w") as wfile:
-    json.dump(filelist, wfile, indent=2)
+sorted_filelist = sorted(filelist, key=lambda dir_file: dir_file[1].lower().lstrip('_'))
+
+with open("../../app/excerptList.ts", encoding="utf-8", mode="w") as wfile:
+    json.dump(sorted_filelist, wfile, indent=2)
+
