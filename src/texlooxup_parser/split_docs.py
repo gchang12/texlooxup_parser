35b3d8c504655b54617060417aa54faaa850972c
gchang12 <gc8@hawaii.edu>
2026-01-06 19:36:15 -1000

    Initialized new modules

---
+++ src/texlooxup_parser/split_docs.py
@@ -0,0 +1,19 @@
+"""
+"""
+
+from typing import (
+    Any,
+    List,
+)
+import re
+
+def split_file(filetext: str) -> List[str]:
+    """
+    """
+
+def parse_section(filetext: str) -> List[dict[Any]]:
+    """
+    """
+
+if __name__ == "__main__":
+    pass
