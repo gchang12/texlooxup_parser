0e6878f2b6345841d8a23f9a09f32dc4182d572e
gchang12 <gc8@hawaii.edu>
2026-01-14 19:41:15 -1000

    Wrote dummy test class for src-compiler

--- tests/test_compile_src.py
+++ tests/test_compile_src.py
@@ -1,4 +1,11 @@
 """
+Tests source compilation module.
 """
 
 import unittest
+
+@unittest.skip("None of the module's code is original and it spans fewer than six lines.")
+class Dummy(unittest.TestCase):
+    """
+    Notifies user of how pointless it is to write a unit test for this module.
+    """
