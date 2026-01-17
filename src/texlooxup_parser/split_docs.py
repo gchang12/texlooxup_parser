2b4ffd5a4fa4630ef3a972f26c85790e332ea07e
gchang12 <gc8@hawaii.edu>
2026-01-17 12:50:10 -1000

    Told script to make subdirectories for each subsection

--- src/texlooxup_parser/split_docs.py
+++ src/texlooxup_parser/split_docs.py
@@ -4,8 +4,13 @@
 from typing import (
     Any,
     List,
+    Iterable,
 )
 import re
+from pathlib import Path
+
+SOURCE_DIR = "./input/impatient/"
+TARGET_DIR = "./output/raw-excerpts/"
 
 def _split_text_by_pattern(filetext: str, pattern: str) -> List[str]:
     """
@@ -41,13 +46,46 @@ def split_descriptions(filetext: str) -> List[str]:
     descriptions = re.findall(pattern, filetext, flags=re.DOTALL)
     return [description.strip() for description in descriptions]
 
-def extract_cts_names(filetext: str) -> str:
+def extract_cts_names(filetext: str) -> List[str]:
     """
     """
     pattern = r"\\cts ([^ ]+?) "
     # search for line containing '\cts'
     cts_names = re.findall(pattern, filetext)
     return cts_names
+
+def extract_cts_lines(filetext: str) -> Iterable[str]:
+    """
+    """
+    pattern = r"\\cts ([^ ]+?) "
+    # search for line containing '\cts'
+    filelines = filetext.splitlines()
+    cts_lines = filter(lambda line: re.search(pattern, line) is not None, filelines)
+    return cts_lines
+
+def is_cts_line(line: str) -> bool:
+    """
+    """
+    pattern = r"\\cts ([^ ]+?) "
+    match = re.search(pattern, line)
+    return match is not None
+
+def get_cts_name(line: str) -> bool:
+    """
+    """
+    pattern = r"\\cts ([^ ]+?) "
+    match = re.search(pattern, line)
+    return match.group(1)
+
+def get_subsection_name(filetext: str) -> str| None:
+    """
+    """
+    first_line = filetext.splitlines()[0]
+    pattern = " {(.+?)}"
+    subsection_match = re.fullmatch(pattern, first_line)
+    if subsection_match is None:
+        return None
+    return subsection_match.group(1)
 
 if __name__ == "__main__":
     TFTI_CHAPTERS = (
@@ -57,3 +95,52 @@ if __name__ == "__main__":
         "pages",
         "paras",
     )
+    def compile_cts_prefixes():
+        """
+        """
+        prefixes = set()
+        for chapter in TFTI_CHAPTERS:
+            filename = SOURCE_DIR + chapter + ".tex"
+            filetext = Path(filename).read_text(encoding="utf-8")
+            cts_lines = extract_cts_lines(filetext)
+            for line in cts_lines:
+                prefix = line[:line.index(" ")]
+                prefixes.add(prefix)
+        return prefixes
+    #prefixes = compile_cts_prefixes()
+    #print(prefixes)
+    def compile_cts_names():
+        """
+        """
+        names = set()
+        for chapter in TFTI_CHAPTERS:
+            filename = SOURCE_DIR + chapter + ".tex"
+            filetext = Path(filename).read_text(encoding="utf-8")
+            cts_lines = extract_cts_lines(filetext)
+            for line in cts_lines:
+                name = get_cts_name(line)
+                names.add(name)
+        return names
+    #names = compile_cts_names()
+    #print(names, len(names))
+    def make_subsection_subdirectories():
+        """
+        """
+        for chapter in TFTI_CHAPTERS:
+            output_dir = Path(TARGET_DIR, chapter)
+            output_dir.mkdir(exist_ok=True, parents=True)
+            filetext = Path(SOURCE_DIR, chapter + ".tex").read_text(encoding="utf-8")
+            sections = split_sections(filetext)
+            #print("\nChapter: ", chapter)
+            #print("=========")
+            for subsection in sections:
+                subsection_name = get_subsection_name(subsection)
+                if subsection_name is None:
+                    continue
+                output_dir.joinpath(subsection_name).mkdir(exist_ok=True, parents=True)
+    make_subsection_subdirectories()
+    # for each file:
+    # split into sections
+    # make new directory for each section
+    # for each section, create subsections as necessary
+    # for each section, write description
